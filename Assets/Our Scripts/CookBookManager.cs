5101cc1b8611a917d214b6e307fa07888a70d745
skonaka6 <78990481+skonaka6@users.noreply.github.com>
2021-10-24 11:44:16 -0700

    Base functionality for CookBook scene implemented

--- Assets/Our Scripts/CookBookManager.cs
+++ Assets/Our Scripts/CookBookManager.cs
@@ -4,15 +4,86 @@ using UnityEngine;
 
 public class CookBookManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public bool DebugMode;
+
+    public GameObject[] pages;
+    private int currPageIndex = 0;
+
+    // Start calls before first frame
     void Start()
     {
+        for ( int i=0; i<pages.Length; i++ )
+        {
+            pages[i].GetComponent<Renderer>().sortingOrder = 0;
+        }
+        currPageIndex = 0;
+        pages[currPageIndex].GetComponent<Renderer>().sortingOrder = 100;
+        DebugModeLog("DebugMode is active...");
+    }
+
+    // Forward page button press
+    // TODO: Check not beyond array boundaries
+    public void PageForward()
+    {
+        DebugModeLog("PageForward()");
+        pages[currPageIndex].GetComponent<Renderer>().sortingOrder = 0;
+        currPageIndex++;
+        if (WithinArrayRange())
+        {
+            pages[currPageIndex].GetComponent<Renderer>().sortingOrder = 100;
+        }else{
+            currPageIndex--;
+            pages[currPageIndex].GetComponent<Renderer>().sortingOrder = 100;
+        }
         
     }
 
-    // Update is called once per frame
-    void Update()
+    // Back page button press
+    // TODO: Check not beyond array boundaries
+    public void PageBack()
     {
-        
+        DebugModeLog("PageBack()");
+        pages[currPageIndex].GetComponent<Renderer>().sortingOrder = 0;
+        currPageIndex--;
+        if (WithinArrayRange())
+        {
+            pages[currPageIndex].GetComponent<Renderer>().sortingOrder = 100;
+        }else{
+            currPageIndex++;
+            pages[currPageIndex].GetComponent<Renderer>().sortingOrder = 100;
+        }
     }
+
+
+    /******************************************************
+    ****************** HELPER FUNCTIONS *******************
+    ******************************************************/
+
+    // Assertion conditional element. Check you do not flip to
+    //  pages beyond the size of the array, or to page -1.
+    bool WithinArrayRange()
+    {
+        if ( (currPageIndex >= 0) && (currPageIndex < pages.Length) )
+        {
+            return true;
+        }else{
+            Debug.Log("No more pages beyond this!");
+            return false;
+        }
+    }
+
+    //Debug mode Debug.Log
+    void DebugModeLog(params string[] s)
+    {
+        if (DebugMode)
+        {
+            string logInput = "";
+            for (int i=0; i<s.Length; i++)
+            {
+                logInput += s[i];
+            }
+            Debug.Log(logInput);
+        }
+    }
+
 }
